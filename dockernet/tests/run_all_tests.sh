44b3f3ffb07bd02a9e937b8826ad87c905810818
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-10-26 17:44:49 +0700

    using bash to test instead of bats

--- dockernet/tests/run_all_tests.sh
+++ dockernet/tests/run_all_tests.sh
@@ -2,7 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # run test files
-BATS=${SCRIPT_DIR}/bats/bats-core/bin/bats
-INTEGRATION_TEST_FILE=${SCRIPT_DIR}/integration_tests.bats 
+INTEGRATION_TEST_FILE=${SCRIPT_DIR}/integration_tests.bash 
 
-$BATS $INTEGRATION_TEST_FILE
+bash $INTEGRATION_TEST_FILE
