4c4287b12d9ec1fef2da72401f7066db7dc0cd00
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-11-07 12:33:30 +0700

    rename

--- dockernet/tests/run_all_tests.sh
+++ dockernet/tests/run_all_tests.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # run test files
-INTEGRATION_TEST_FILE=${SCRIPT_DIR}/integration_tests.bash 
+INTEGRATION_TEST_FILE=${SCRIPT_DIR}/integration_tests.sh
 
 bash $INTEGRATION_TEST_FILE
